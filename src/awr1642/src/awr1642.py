89b2886d13d09ae27044e7e380eeccb36b27b211
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2019-02-09 14:31:02 +0100

    Captured point cloud data into awr1642 node

--- src/awr1642/src/awr1642.py
+++ src/awr1642/src/awr1642.py
@@ -4,17 +4,27 @@
 ##################IMPORTS#########################
 from __future__ import division
 import rospy
-import sys
+import roslib; 
+import sys 
 from std_msgs.msg import String
+from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import *
-from math import *
+from sensor_msgs.msg import *
+#from point_cloud2 import *
 from numpy import *
+#from matplotlib.pyplot import *
+from time import *
 
 
 class awr1642():
 
 	def __init__(self):
 		print "awr1642 running..."
+		self.pointCloud2_subscriber=rospy.Subscriber("/mmWaveDataHdl/RScan",PointCloud2,self.pointCloud2_cb)
+
+
+	def pointCloud2_cb(self,pointCloud2_data):
+		print pointCloud2_data
 
 
 
